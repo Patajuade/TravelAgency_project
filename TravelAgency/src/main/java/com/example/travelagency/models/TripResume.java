79c6a9362b5a12542c821fbead408ba53ba4ef7d
Helhathomcoet <93981789+Helhathomcoet@users.noreply.github.com>
2022-04-29 16:47:22 +0200

    FIrst update

---
+++ TravelAgency/src/main/java/com/example/travelagency/models/TripResume.java
@@ -0,0 +1,8 @@
+package com.example.travelagency.models;
+
+import java.util.ArrayList;
+
+public class TripResume {
+    ArrayList<TripStage> stages;
+    CityModel Source;
+}
---
+++ TravelAgency/src/main/java/com/example/travelagency/models/TripsResume.java
@@ -0,0 +1,9 @@
+package com.example.travelagency.models;
+
+import java.util.ArrayList;
+
+public class TripsResume {
+    ArrayList<TripResume> trips;
+
+
+}
