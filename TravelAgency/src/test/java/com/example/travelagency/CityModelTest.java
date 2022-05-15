85a357c74afd546ba29e6841000eec0af2f0a5de
Patajuade <manon.diana@icloud.com>
2022-05-15 22:53:28 +0200

    add distanceCompute_WhenCitiesAreTheSamePosition and distanceCompute_WhenCitiesArentTheSamePosition unit test

--- TravelAgency/src/test/java/com/example/travelagency/CityModelTest.java
+++ TravelAgency/src/test/java/com/example/travelagency/CityModelTest.java
@@ -8,6 +8,29 @@ public class CityModelTest {
     @Test
     public void getCityName(){
         CityModel city = new CityModel("test","0","0","test");
-        assertTrue(city.getCityName().equals("test"));
+        assertEquals(city.getCityName(),"test");
+        assertNotEquals(city.getCityName(),"0");
     }
+    //TODO : pour distanceCompute
+    //tester pour 2 villes identiques (même latitude et même longitude)
+
+    @Test
+    public void distanceCompute_WhenCitiesAreTheSamePosition(){
+        CityModel city1 = new CityModel("city1","0","0","test");
+        CityModel city2 = new CityModel("city2","0","0","test");
+        double distance = city1.distanceCompute(city2);
+        assertEquals(distance,0);
+        assertEquals(city2.distanceCompute(city1),distance);
+    }
+
+    @Test
+    public void distanceCompute_WhenCitiesArentTheSamePosition(){
+        CityModel city1 = new CityModel("city1","100","100","test");
+        CityModel city2 = new CityModel("city2","0","0","test");
+        double distance = city1.distanceCompute(city2);
+        System.out.println(distance);
+        assertEquals(distance,9815.41);
+        assertEquals(city2.distanceCompute(city1),distance);
+    }
+
 }
