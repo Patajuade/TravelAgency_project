93806e66f090ae8e32514cb513d3412b552f5f42
Patajuade <manon.diana@icloud.com>
2022-05-19 00:01:35 +0200

    add updateTripStage for TripResume and update original method

--- TravelAgency/src/test/java/com/example/travelagency/TripResumeTest.java
+++ TravelAgency/src/test/java/com/example/travelagency/TripResumeTest.java
@@ -5,22 +5,26 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TripResumeTest {
 
     private TripResume tripResume;
-    private static HotelStage hotelStage;
-    private static PlaneStage planeStage;
-    private static CityModel destination;
-    private static CityModel source;
+    private HotelStage hotelStage;
+    private PlaneStage planeStage;
+    private CityModel destination;
+    private CityModel source;
 
-    @BeforeAll
-    public static void setUp(){
+    @BeforeEach
+    public void setUp(){
+        tripResume = new TripResume();
         hotelStage = new HotelStage();
         planeStage = new PlaneStage();
-        destination = new CityModel("destination","10","10","a");
-        source = new CityModel("source","20","20","b");
+        destination = new CityModel("destination","10","10","destinationCountry");
+        source = new CityModel("source","20","20","sourceCountry");
 
         hotelStage.setDuration(5);
         hotelStage.setDistance(1000);
@@ -35,9 +39,20 @@ public class TripResumeTest {
         planeStage.setSource(source);
     }
 
-    @BeforeEach
-    public void setUp_beforeEach(){
-        tripResume = new TripResume();
+    @Test
+    public void addStage(){
+        tripResume.addStage(planeStage);
+        tripResume.addStage(hotelStage);
+        ArrayList<TripStage> stages = tripResume.getStages();
+        assertEquals(stages.size(),2);
+    }
+
+    @Test
+    public void removeStage(){
+        tripResume.removeStage(planeStage);
+        tripResume.removeStage(hotelStage);
+        ArrayList<TripStage> stages = tripResume.getStages();
+        assertEquals(stages.size(),0);
     }
 
     @Test
@@ -71,6 +86,21 @@ public class TripResumeTest {
         assertEquals(tripResume.getTotalTime(),10);
         assertEquals(tripResume.getTotalDistance(),3089.52);
         assertEquals(tripResume.getTotalPrice(),40);
+    }
+
+    @Test
+    public void updateTripStage(){
+        tripResume.setSource(source);
+        tripResume.addStage(planeStage);
+        PlaneStage planeStage1 = new PlaneStage();
+        planeStage1.setDuration(5);
+        planeStage1.setDistance(1000);
+        planeStage1.setPrice(20);
+        planeStage1.setDestination(source); //car voyage aller-retour
+        tripResume.addStage(planeStage1);
+        tripResume.updateTripStep();
+
+        assertEquals(tripResume.getStages().get(1).getSource().getCityName(),tripResume.getStages().get(0).getDestination().getCityName());
     }
 
     @AfterEach
