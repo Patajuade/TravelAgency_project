f319808e72231815f48fd92fbd191f4eb38302bf
Patajuade <manon.diana@icloud.com>
2022-05-20 02:17:51 +0200

    add hotelStage start of javadoc

--- TravelAgency/src/main/java/com/example/travelagency/models/HotelStage.java
+++ TravelAgency/src/main/java/com/example/travelagency/models/HotelStage.java
@@ -1,36 +1,57 @@
 package com.example.travelagency.models;
 
-import javafx.fxml.FXMLLoader;
-
+/**
+ *
+ */
 public class HotelStage extends TripStage {
-
     int numberOfNights;
-
     int pricePerNight;
 
+    /**
+     *
+     */
     public HotelStage() {
         distance = 0;
     }
 
+    /**
+     *
+     */
     @Override
     public void priceCompute() {
         price = pricePerNight * numberOfNights;
     }
+
+    /**
+     *
+     */
     @Override
     public void durationCompute() {
         duration = numberOfNights * 24;
     }
 
+    /**
+     * @return
+     */
     public int getNumberOfNights() {
         return numberOfNights;
     }
 
+    /**
+     * @return
+     */
     public int getPricePerNight() { return pricePerNight; }
 
+    /**
+     * @param numberOfNights
+     */
     public void setNumberOfNights(int numberOfNights) {
         this.numberOfNights = numberOfNights;
     }
 
+    /**
+     * @param pricePerNight
+     */
     public void setPricePerNight(int pricePerNight) {
         this.pricePerNight = pricePerNight;
     }
