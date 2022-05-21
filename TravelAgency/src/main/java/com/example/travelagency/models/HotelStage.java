517f94f7c4a68a4cddee7ecfff098d4a016c6b18
Patajuade <manon.diana@icloud.com>
2022-05-21 18:30:30 +0200

    add HotelStage javadoc

--- TravelAgency/src/main/java/com/example/travelagency/models/HotelStage.java
+++ TravelAgency/src/main/java/com/example/travelagency/models/HotelStage.java
@@ -1,21 +1,23 @@
 package com.example.travelagency.models;
 
 /**
- *
+ *<p>Model for hotel objects</p>
+ * inherits of TripStage class
  */
 public class HotelStage extends TripStage {
     int numberOfNights;
     int pricePerNight;
 
     /**
-     *
+     *<p>default constructor</p>
      */
     public HotelStage() {
         distance = 0;
     }
 
     /**
-     *
+     *<p>Calculates the stay price by multiplying the price per night and the number of nights.</p>
+     *Overrides method in the mother class.
      */
     @Override
     public void priceCompute() {
@@ -23,7 +25,8 @@ public class HotelStage extends TripStage {
     }
 
     /**
-     *
+     *<p>Calculates the number of days for the hotel stay, by multiplying the number of nights by 24.</p>
+     * Overrides method in the mother class.
      */
     @Override
     public void durationCompute() {
@@ -31,26 +34,30 @@ public class HotelStage extends TripStage {
     }
 
     /**
-     * @return
+     * <p>Getter allowing to use numberOfNihts outside of this class.</p>
+     * @return number of nights spent at the hotel
      */
     public int getNumberOfNights() {
         return numberOfNights;
     }
 
     /**
-     * @return
+     * <p>Getter allowing to use pricePerNights outside of this class.</p>
+     * @return price that a night at the hotel costs
      */
     public int getPricePerNight() { return pricePerNight; }
 
     /**
-     * @param numberOfNights
+     * <p>Setter allowing to set a new value for numberOfNights.</p>
+     * @param numberOfNights is the number of nights.
      */
     public void setNumberOfNights(int numberOfNights) {
         this.numberOfNights = numberOfNights;
     }
 
     /**
-     * @param pricePerNight
+     * <p></p>
+     * @param pricePerNight is the price per night.
      */
     public void setPricePerNight(int pricePerNight) {
         this.pricePerNight = pricePerNight;
