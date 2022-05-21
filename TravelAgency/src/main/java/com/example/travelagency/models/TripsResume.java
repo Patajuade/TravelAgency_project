f5f9c5453467c73ea34dc26f6ad3fff89e88760b
Patajuade <manon.diana@icloud.com>
2022-05-21 22:38:39 +0200

    add TripsResume Javadoc

--- TravelAgency/src/main/java/com/example/travelagency/models/TripsResume.java
+++ TravelAgency/src/main/java/com/example/travelagency/models/TripsResume.java
@@ -1,18 +1,31 @@
 package com.example.travelagency.models;
-
 import java.util.ArrayList;
 
+/**
+ * Class to make a list of TripResume, which are complete single trips, each trip containing multiple HotelStages and PlaneStages.
+ */
 public class TripsResume {
     ArrayList<TripResume> trips;
 
+    /**
+     * Default constructor
+     */
     public TripsResume() {
         trips = new ArrayList<>();
     }
 
+    /**
+     * Method allowing to add a trip to the trips list
+     * @param tripResume is a complete trip
+     */
     public void addTripResume(TripResume tripResume){
         trips.add(tripResume);
     }
 
+    /**
+     * Method allowing to remove a trip from the trips list
+     * @param tripResume is a complete trip
+     */
     public void removeTripResume(TripResume tripResume){
         trips.remove(tripResume);
     }
