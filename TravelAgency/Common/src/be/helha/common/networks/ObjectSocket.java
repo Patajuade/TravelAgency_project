d6f17ffb70bf1b04e2c0dfa83d4a8eb11e401739
Patajuade <manon.diana@icloud.com>
2022-05-23 22:19:07 +0200

    add common - networks and Server javadoc

--- TravelAgency/Common/src/be/helha/common/networks/ObjectSocket.java
+++ TravelAgency/Common/src/be/helha/common/networks/ObjectSocket.java
@@ -5,6 +5,9 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+/**
+ * Object socket manager
+ */
 public class ObjectSocket implements AutoCloseable{
     private ObjectOutputStream out;
     private ObjectInputStream in;
@@ -14,12 +17,24 @@ public class ObjectSocket implements AutoCloseable{
         in = new ObjectInputStream(socket.getInputStream());
     }
 
+    /**
+     * Writes the object
+     * @param object written object
+     * @throws IOException management of input/output exceptions.
+     */
     public void write(Object object) throws IOException {
         out.reset();
         out.writeObject(object);
         out.flush();
     }
 
+    /**
+     * Reads an object
+     * @param <T> generic parameter
+     * @return Read object
+     * @throws IOException management of input/output exceptions.
+     * @throws ClassNotFoundException occurs when the JVM tries to load a particular class but does not find it in the classpath
+     */
     public <T> T read() throws IOException, ClassNotFoundException {
         return (T) in.readObject();
     }
